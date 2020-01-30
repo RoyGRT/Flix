de2d653ffd46e6123402eb20e37c9994f02a7fff
Ruotian Gao <39045851+RoyGRT@users.noreply.github.com>
2020-01-30 00:34:42 -0800

    Add files via upload

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by 高若天 on 1/29/20.
+//  Copyright © 2020 grt66888@163.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by 高若天 on 1/29/20.
+//  Copyright © 2020 grt66888@163.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
